19c6226370d62d46fb4b20aa3af1ed2333dfc2a0
Joshua Gorospe <joshua.gorospe@gmail.com>
2024-04-02 16:43:08 +0000

    Add current status

---
+++ Lesson2CodeSessions_SimpleStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+/**/
+contract SimpleStorage {
+
+   bool  favoriteBool = false;
+   uint256  favoriteNumber = 8;
+   string  goodAdvice = "Friends don't let friends trade meme tokens while drunk....";
+   int256  favoriteInt = -777;
+   address  eightsixsevenfivethreeOhNine =  0x0000000000000000000000000000000000000000;
+   bytes  forAGoodTimeCall = "jen";
+  
+}
+
+// Note to self : 
+// The data-type string is an array of bytes.
